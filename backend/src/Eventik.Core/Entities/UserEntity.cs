089560975f129adbf55c3c67e18a75dd5674ad13
Viktor Yelenetskyi <108831174+Alcon-0@users.noreply.github.com>
2025-06-27 15:27:16 +0300

    feat: add user management entities and preference types

---
+++ backend/src/Eventik.Core/Entities/UserEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eventik.Core.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; init; }
+
+    [Required, EmailAddress, MaxLength(255)]
+    public string Email { get; init; } = string.Empty;
+
+    [Required, MaxLength(128)] 
+    public string PasswordHash { get; init; } = string.Empty;
+
+    [Required]
+    public string City { get; init; } = string.Empty;
+
+    public string? Name { get; init; }
+    public string? Surname { get; init; }
+    
+    public ICollection<UserPreferenceTag> Preferences { get; init; } = new List<UserPreferenceTag>();
+}
