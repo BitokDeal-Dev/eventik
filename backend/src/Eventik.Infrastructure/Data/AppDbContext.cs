7e1814f5a2ae6b51a9f4cd71d6c91de6691a8edf
Viktor Yelenetskyi <108831174+Alcon-0@users.noreply.github.com>
2025-06-27 15:27:49 +0300

    feat: add AppDbContext and dependency injection for infrastructure services

---
+++ backend/src/Eventik.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Eventik.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventik.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; } = null!;
+    public DbSet<UserPreferenceTag> UserPreferenceTags { get; set; } = null!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserPreferenceTag>()
+            .Property(p => p.Type)
+            .HasConversion<string>();
+
+        modelBuilder.Entity<UserPreferenceTag>()
+            .HasIndex(p => p.Type)
+            .IsUnique();
+    }
+}
---
+++ backend/src/Eventik.Infrastructure/ServiceConfiguration/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Eventik.Core.Interfaces;
+using Eventik.Infrastructure.Data;
+using Eventik.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eventik.Infrastructure.ServiceConfiguration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string? connectionString)
+    {
+        services.AddDbContext<AppDbContext>(options => 
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+        
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IUserRepository, UserRepository>();
+        
+        return services;
+    }
+}
